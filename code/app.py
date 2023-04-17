61e2423726aeec2e13df23266005133efe9587f7
Rajkumar S <rajkumaar2304@icloud.com>
2023-04-17 13:06:59 -0700

    use google search results

--- code/app.py
+++ code/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, request, make_response
-import pickle
 import spacy
 import json
 from googlesearch import search
@@ -8,18 +7,13 @@ from bs4.element import Comment
 import requests
 from time import sleep
 from collections import Counter
-from duckduckgo_search import ddg_news
 
 app = Flask(__name__)
 nlp = spacy.load("en_core_web_sm")
 
 
-# pickle_filename = "dataset.pickle"
-# with open(pickle_filename, "rb") as f:
-#     dataset = pickle.load(f)
-
 def tag_visible(element):
-    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+    if element.parent.name in ['style', 'script', 'head', 'meta', '[document]']:
         return False
     if isinstance(element, Comment):
         return False
@@ -29,8 +23,19 @@ def tag_visible(element):
 def text_from_html(body):
     soup = BeautifulSoup(body, 'html.parser')
     texts = soup.findAll(text=True)
-    visible_texts = filter(tag_visible, texts)  
+    visible_texts = filter(tag_visible, texts)
     return u" ".join(t.strip() for t in visible_texts)
+
+
+def get_title_and_image(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    if response.status_code != 200 or soup.title == None:
+        return (None, None)
+    title = soup.title.string
+    image = soup.find("meta", property="og:image")
+    return (title, image['content'] if image else 'https://placehold.co/100x55')
+
 
 def get_recommendations(urls):
     recommendations = []
@@ -38,15 +43,13 @@ def get_recommendations(urls):
         entities = []
         print("Processing " + url)
         response = requests.get(url)
-        # soup = BeautifulSoup(response.content, 'html.parser')
         for entity in nlp(text_from_html(response.content)).ents:
             if entity.label_ in ['PERSON', 'ORG', 'GPE']:
                 entity_text = ''.join(ch for ch in entity.text if ch.isalnum())
                 if len(entity_text) > 2:
                     entities.append(entity_text)
 
-        
-        top_entities = [entity for entity, count in Counter(entities).most_common(10)]
+        top_entities = [entity for entity, count in Counter(entities).most_common(20)]
         print("Entities for " + url + " found : " + json.dumps(top_entities))
 
         related_entities = []
@@ -62,43 +65,33 @@ def get_recommendations(urls):
             if not ('related' in data):
                 continue
 
-            related_entities += [item['@id'].split('/')[-1] for item in data['related'] if item['weight'] > 0.65]
+            related_entities += [item['@id'].split('/')[-1]
+                                 for item in data['related'] if item['weight'] > 0.5][:3]
             # print("Related : " + json.dumps(related_entities))
 
-        top_related_entities = [entity for entity, count in Counter(related_entities).most_common(10)]
-        print("Top related entities for " + url + " found : " + json.dumps(top_related_entities))
+        top_related_entities = [entity for entity, count in Counter(
+            related_entities).most_common(10)]
+        print("Top related entities for " + url +
+              " found : " + json.dumps(top_related_entities))
 
-        for entity in related_entities:
-            query = f"{entity}"
-            # results = search('"' + query + '"', num_results=5, sleep_interval=1)
-            response = ddg_news(query)
-            # print(response)
+        for entity in top_related_entities:
             results = []
-            if response is None or len(response) < 1:
-                continue
-            for item in response:
-                if 'url' in item:
-                    results.append({
-                        'url': item['url'],
-                        'title': item['title']
+            for result in search(entity + ' News', num=10, stop=2, pause=2, extra_params={'tbm': 'nws'}):
+                if result.count("/") > 3:
+                    title, image = get_title_and_image(result)
+                    if title == None:
+                        continue
+                    recommendations.append({
+                        'url': result,
+                        'title': title,
+                        'image': image
                     })
-            i = 0
-            for result in results:
-                if i == 5:
-                    break
-                # print(result)
-                if result['url'].count("/") > 3:
-                    recommendations.append(result)
-                    # print("Added " + result)
-                    i += 1
-            # print("Searched for " + entity)
-            # print(recommendations)
-        top_recommendations = [json.loads(recommendation) for recommendation, count in Counter(json.dumps(l) for l in recommendations).most_common(10)]
+            print("Searched for " + entity)
+        top_recommendations = [json.loads(recommendation) for recommendation, count in Counter(
+            json.dumps(l) for l in recommendations).most_common(10)]
     print(top_recommendations)
     return top_recommendations
 
-urls = ["https://paypal.com"]
-# print(get_recommendations(urls))
 
 @app.route("/recommendations", methods=["POST", "OPTIONS"])
 def recommendations():
@@ -108,9 +101,10 @@ def recommendations():
         data = request.json
         urls = data["history"]
         recommendations = {
-            'recommendations' : get_recommendations(urls)
+            'recommendations': get_recommendations(urls)
         }
         return _corsify_actual_response(jsonify(recommendations))
+
 
 def _build_cors_preflight_response():
     response = make_response()
@@ -118,6 +112,7 @@ def _build_cors_preflight_response():
     response.headers.add('Access-Control-Allow-Headers', "*")
     response.headers.add('Access-Control-Allow-Methods', "*")
     return response
+
 
 def _corsify_actual_response(response):
     response.headers.add("Access-Control-Allow-Origin", "*")
